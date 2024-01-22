25dcbb8f62cc6a83638d8e5c6435e72ea6a1a3a1
HoadvRP <hoadv@reactplus.jp>
2024-01-22 15:23:04 +0700

    done product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,26 @@ export default {
         Karla: ["Karla", "sans-serif"],
       },
       colors: {
-        "light-coffee": "#C89F94",
+        coffee: {
+          50: "#E8D6D0",
+          200: "#C89F94",
+          400: "#A25F4B",
+          600: "#744838",
+        },
       },
       keyframes: {
         slideDown: {
           "0%": { transform: "translateY(-100%)" },
           "100%": { transform: "translateY(0)" },
         },
+        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 } },
       },
       animation: {
         slideDown: "slideDown 0.5s ease-in-out",
+        fadeIn: "fadeIn 0.5s ease-in-out",
+      },
+      backgroundImage: {
+        "slider-bg": 'url("./slider-bg.jpeg")',
       },
     },
   },
