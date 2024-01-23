7cb1206af3e6d830a7466a6dd1a8e6f12c07980f
HoadvRP <hoadv@reactplus.jp>
2024-01-23 09:25:22 +0700

    save draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 export default {
   content: ["./index.html"],
   theme: {
-    theme: {
-      screens: {
-        xs: "480px",
-        ...defaultTheme.screens,
-      },
-    },
-    extend: {
-      fontFamily: {
-        Karla: ["Karla", "sans-serif"],
-      },
-      colors: {
-        coffee: {
-          50: "#E8D6D0",
-          200: "#C89F94",
-          400: "#A25F4B",
-          600: "#744838",
-        },
-      },
-      keyframes: {
-        slideDown: {
-          "0%": { transform: "translateY(-100%)" },
-          "100%": { transform: "translateY(0)" },
-        },
-        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 } },
-      },
-      animation: {
-        slideDown: "slideDown 0.5s ease-in-out",
-        fadeIn: "fadeIn 0.5s ease-in-out",
-      },
-      backgroundImage: {
-        "slider-bg": 'url("./slider-bg.jpeg")',
-      },
-    },
+    // extend: {
+    //   spacing: {
+    //     big: "48rem",
+    //   },
+    // },
+    // screens: {
+    //   sm: "480px",
+    //   md: "768px",
+    //   lg: "1024px",
+    //   xl: "1280px",
+    // },
+    // fontFamily: {
+    //   nunito: ["Nunito", "sans-serif"],
+    // },
   },
   plugins: [],
 };
